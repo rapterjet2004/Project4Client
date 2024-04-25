097aad35ef68b44d42f4aaa55c43e63e62e6d858
ShaneSom <ssoms2@uic.edu>
2024-04-25 00:32:56 -0500

    Changed Message class to better suit battleship

--- src/main/java/Message.java
+++ src/main/java/Message.java
@@ -1,5 +1,35 @@
+
 import java.io.Serializable;
+import javafx.util.Pair;
+
 
 public class Message implements Serializable {
     static final long serialVersionUID = 42L;
+
+    boolean isPlacingShips = false;
+    boolean isAttacking = false;
+    boolean didHit = false;
+    int[][] updateGrid;
+    Pair<Integer,Integer> attackCoord;
+
+
+    public void setUpdateGrid(int[][] updatedGrid){
+        updateGrid = updatedGrid;
+
+    }
+
+    public int[][] getUpdateGrid(){
+        return updateGrid;
+    }
+
+    public void setAttackCoord(Pair<Integer,Integer> coords){
+        attackCoord = coords;
+    }
+
+    public Pair<Integer,Integer> getAttackCoord(){
+        return attackCoord;
+    }
+
+
+
 }
